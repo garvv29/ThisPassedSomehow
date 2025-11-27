a61a9d90289513ca62631b7966f51ac37f0f6d88
garvv29 <garv.choure2023@ssipmt.com>
2025-11-27 18:17:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.1 MB (85.01%)

---
+++ my-folder/0812-rotate-string/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        // Strings must be the same length to be rotations of each other
+        if (s.length() != goal.length()) return false;
+        
+        // Concatenate s with itself and check if goal exists in it
+        string doubledS = s + s;
+        return doubledS.find(goal) != string::npos;
+    }
+};
