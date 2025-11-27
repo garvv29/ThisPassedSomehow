c048495e971e1f37a1573167346b6ef56c1ffc88
garvv29 <garv.choure2023@ssipmt.com>
2025-11-27 17:57:42 +0000

    [LeetCode Sync] Runtime - 4 ms (22.37%), Memory - 17.9 MB (37.67%)

---
+++ my-folder/2032-largest-odd-number-in-string/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        int i=0;
+        for(i=num.length()-1;i>=0;i--)
+        {
+            if((num[i]-'0')%2!=0)
+            {
+                break;
+            }
+        }
+        return num.substr(0,i+1);
+    }
+};
