3c6e481b98ae26cd93360c25afae663f754c4388
garvv29 <garv.choure2023@ssipmt.com>
2025-11-27 20:48:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.7 MB (91.23%)

--- my-folder/0073-set-matrix-zeroes/solution.cpp
+++ my-folder/0073-set-matrix-zeroes/solution.cpp
@@ -1,26 +1,56 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        bool zeroinFirstCol = false;
-        for (int row = 0; row < matrix.size(); row++) {
-            if (matrix[row][0] == 0) zeroinFirstCol = true;
-            for (int col = 1; col < matrix[0].size(); col++) {
-                if (matrix[row][col] == 0) {
-                    matrix[row][0] = 0;
-                    matrix[0][col] = 0;
+        bool colzero = false, rowzero = false;
+
+        // Check first row
+        for (int j = 0; j < matrix[0].size(); j++) {
+            if (matrix[0][j] == 0) {
+                rowzero = true;
+                break;
+            }
+        }
+
+        // Check first column
+        for (int i = 0; i < matrix.size(); i++) {
+            if (matrix[i][0] == 0) {
+                colzero = true;
+                break;
+            }
+        }
+
+        // Mark rows & columns
+        for (int i = 1; i < matrix.size(); i++) {
+            for (int j = 1; j < matrix[0].size(); j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
 
-        for (int row = matrix.size() - 1; row >= 0; row--) {
-            for (int col = matrix[0].size() - 1; col >= 1; col--) {
-                if (matrix[row][0] == 0 || matrix[0][col] == 0) {
-                    matrix[row][col] = 0;
+        // Fill matrix (except first row/col)
+        for (int i = 1; i < matrix.size(); i++) {
+            for (int j = 1; j < matrix[0].size(); j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
                 }
             }
-            if (zeroinFirstCol) {
-                matrix[row][0] = 0;
+        }
+
+        // Zero out first row if needed
+        if (rowzero) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Zero out first column if needed
+        if (colzero) {
+            for (int i = 0; i < matrix.size(); i++) {
+                matrix[i][0] = 0;
             }
         }
     }
 };
+
