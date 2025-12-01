b6ada1bcc121d45684edbd35d517a5be7f5a3b4c
garvv29 <garv.choure2023@ssipmt.com>
2025-12-01 21:12:30 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 23.7 MB (98.52%)

--- my-folder/0283-move-zeroes/solution.cpp
+++ my-folder/0283-move-zeroes/solution.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-      int j=-1;
-      for(int i=0;i<nums.size();i++)
-      {
-        if(nums[i]==0)
-        {
-            j=i;
-            break;
+        int j = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
         }
-      }  
-      if(j==-1)return;
-      for(int i=j+1;i<nums.size();i++)
-      {
-        if(nums[i]!=0)
-        {
-            swap(nums[j],nums[i]);
-            j++;
-        }
-      }
     }
 };
+
