6b1f9ae71e5672e2c9dad93c6195e02bd230935a
garvv29 <garv.choure2023@ssipmt.com>
2026-01-23 19:15:40 +0000

    [LeetCode Sync] Runtime - 55 ms (31.70%), Memory - 90.7 MB (71.47%)

--- my-folder/0217-contains-duplicate/solution.cpp
+++ my-folder/0217-contains-duplicate/solution.cpp
@@ -1,43 +1,12 @@
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) { 
-
-    sort(nums.begin(),nums.end()) ;
-
-     for (int i=0;i<nums.size()-1;i++) {
-
-          if (nums[i]==nums[i+1]) 
-
-           {  return true;
-           }   
-           
-         }
-             return false ;
-     
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for(int n:nums)
+        {
+            if(s.count(n))return true;
+            s.insert(n);
+        }
+        return false;
     }
 };
-
-
-
-
-
-
-
- /*Using unordered set (HashSet)
-    std::unordered_set<int> myset ;
-
-    for (int i=0; i<nums.size(); i++)
-
-    {
-              if (myset.count(nums[i]) == true)
-              {
-                return true ;
-              }
-               
-             else myset.insert(nums[i]);  
-
-    }     
-      
-
-    return false;
-    */
