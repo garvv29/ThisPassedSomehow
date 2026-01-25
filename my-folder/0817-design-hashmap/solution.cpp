7c2fa7043c0b1fe0ad2bd474f3babb7133876db1
garvv29 <garv.choure2023@ssipmt.com>
2026-01-25 20:39:51 +0000

    [LeetCode Sync] Runtime - 76 ms (17.22%), Memory - 58.2 MB (96.53%)

--- my-folder/0817-design-hashmap/solution.cpp
+++ my-folder/0817-design-hashmap/solution.cpp
@@ -1,30 +1,34 @@
 class MyHashMap {
-    private:
-    vector<int> hash_map;
 public:
-    MyHashMap() {
-      hash_map.resize(1e6+1,-1);  
-    }
-    
+    vector<pair<int,int>> hm; 
+
+    MyHashMap() {}
+
     void put(int key, int value) {
-        hash_map[key]=value;
+        for (auto &i : hm) { 
+            if (i.first == key) {
+                i.second = value;
+                return;
+            }
+        }
+        hm.push_back({key, value}); 
     }
-    
+
     int get(int key) {
-        return hash_map[key];
+        for (auto &i : hm) {
+            if (i.first == key)
+                return i.second;
+        }
+        return -1;
     }
-    
+
     void remove(int key) {
-        hash_map[key]=-1;
+        for (auto it = hm.begin(); it != hm.end(); ++it) {
+            if (it->first == key) {
+                hm.erase(it);
+                return;
+            }
+        }
     }
 };
 
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap* obj = new MyHashMap();
- * obj->put(key,value);
- * int param_2 = obj->get(key);
- * obj->remove(key);
- */
-
-   auto init = atexit([]() { ofstream("display_runtime.txt") << "0";});
